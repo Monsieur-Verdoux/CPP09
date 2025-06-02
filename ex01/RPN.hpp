0551bafea4246c0ad512de8b7196535c14ea4887
Monsieur-Verdoux <andrey.kovalevskiy89@gmail.com>
2025-06-02 14:37:58 +0300

    removed unneeded inclusion in ex01

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -14,21 +14,18 @@
 
 #include <iostream>
 #include <sstream>
-#include <string>
 #include <stack>
-#include <stdexcept>
-#include <cctype> // for std::isdigit
 
 class RPN
 {
 	private:
 		std::string _inputString; 
-		void validateInput() const; // validate the input string
+		void validateInput() const;
 	public:
 		RPN(); 
 		RPN(const std::string &inputString); 
-		RPN(const RPN &original); // copy constructor
+		RPN(const RPN &original);
 		RPN &operator=(const RPN &original); 
 		~RPN(); 
-		int calculateRPN() const; // calculate the RPN expression, const is added to indicate that this method does not modify the object
+		int calculateRPN() const;
 };
